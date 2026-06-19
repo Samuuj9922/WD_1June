98eacd25d3d0f081355f977f7345054f395ac7c6
Samuuj9922 <jagtapsamu610@gmail.com>
2026-06-19 11:53:42 +0530

    day14

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+// output method
+     console.log("This is console");
+     alert("This is alert")
+     confirm("This is confirm")
+     document.write("This is document")
+     document.writeln("This is new document")
+     
+     //Input Method
+
+     let a= prompt("Enter your Name:")
+     document.writeln("Your name is:",a)
---
+++ JS/string.js
@@ -0,0 +1,94 @@
+//string methods -> ' ' , " " , "' '" .
+
+let a="Fortune"
+console.log(a);
+console.log(typeof(a));
+
+
+let b=a.charAt(3)
+console.log(b);
+
+let c=a.charCodeAt(3)
+console.log(c);
+
+let d=a.endsWith("e")
+console.log(d);
+
+let e=a.startsWith("F")
+ console.log(e);
+
+let f=a.includes('z')
+console.log(f);
+
+let g=a.toLocaleUpperCase()
+console.log(g);
+
+let h=a.toLocaleLowerCase()
+console.log(h);
+
+let i=a.concat( " cloud")
+console.log(i);
+
+let j=a.replace("Fortune","Cravita")
+console.log(j);
+
+let k=a.repeat(5)
+console.log(k);
+
+
+// Number Method -> 34 56.78
+
+let ab=34;
+console.log(ab);
+console.log(typeof(ab));
+
+let xy=67.89868743435
+console.log(xy);
+console.log(typeof(xy));
+
+
+let yz=xy.toFixed(2)
+console.log(yz);
+
+let uv=ab.toString()
+console.log(uv)
+console.log(typeof(uv));
+
+
+//Boolean
+
+let m=true
+console.log(typeof(m));
+
+let x=false
+console.log(typeof(x));
+
+// null
+
+let n=null
+console.log(typeof(n));
+
+let p;
+console.log(p);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
