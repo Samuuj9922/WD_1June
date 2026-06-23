7a487c7162d9a90a6f4633568e79a36d97050b16
Samuuj9922 <jagtapsamu610@gmail.com>
2026-06-23 18:38:39 +0530

    day16

--- JS/function.js
+++ JS/function.js
@@ -44,3 +44,28 @@ let square=((r,s)=>{
     
 })
 square(5,5)
+
+
+// 6. Nested Function
+
+function grandparent()
+{
+    console.log("I am GP");
+    
+    function parent()
+    {
+        console.log("I am parent");
+
+    
+
+    function child()
+    {
+        console.log("I am child");
+        
+    }
+    return child
+}
+ return parent
+    
+}
+grandparent()()()
